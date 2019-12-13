093d508d835dee4bb613488585a6e84fb4640f4c
fastandfurios <semu-dima@yandex.ru>
2019-12-13 14:37:20 +0300

    Добавлен аккордион

---
+++ Yakutia/Yakutia/CustomControls/Accordion.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Yakutia.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class Accordion : ContentView
+    {
+        /// <summary>
+        /// Задает представление для индикатора аккордиона
+        /// </summary>
+        public static readonly BindableProperty IndicatorViewProperty = BindableProperty.Create(nameof(IndicatorView), typeof(View), typeof(Accordion), default(View));
+        public View IndicatorView
+        {
+            get => (View)GetValue(IndicatorViewProperty);
+            set => SetValue(IndicatorViewProperty, value);
+        }
+
+        /// <summary>
+        /// Задает представление для контента внутри аккордиона
+        /// </summary>
+        public static readonly BindableProperty ContentViewProperty = BindableProperty.Create(nameof(AccordionContentView), typeof(View), typeof(Accordion), default(View));
+        public View AccordionContentView
+        {
+            get => (View)GetValue(ContentViewProperty);
+            set => SetValue(ContentViewProperty, value);
+        }
+
+        /// <summary>
+        /// Задает заголовок аккордиона
+        /// </summary>
+        public static readonly BindableProperty TitleViewProperty = BindableProperty.Create(nameof(TitleView), typeof(string), typeof(Accordion), default(string));
+        public string TitleView
+        {
+            get => (string)GetValue(TitleViewProperty);
+            set => SetValue(TitleViewProperty, value);
+        }
+
+        public static readonly BindableProperty IsOpenBindablePropertyProperty = BindableProperty.Create(nameof(IsOpen), typeof(bool), typeof(Accordion), false, propertyChanged: IsOpenChanged);
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenBindablePropertyProperty); }
+            set { SetValue(IsOpenBindablePropertyProperty, value); }
+        }
+
+        /// <summary>
+        /// Задает фон выпадающего аккордиона
+        /// </summary>
+        public static readonly BindableProperty HeaderBackgroundColorProperty = BindableProperty.Create(nameof(HeaderBackgroundColor), typeof(Color), typeof(Accordion), Color.Transparent);
+        public Color HeaderBackgroundColor
+        {
+            get { return (Color)GetValue(HeaderBackgroundColorProperty); }
+            set { SetValue(HeaderBackgroundColorProperty, value); }
+        }
+
+
+        /// <summary>
+        /// Управляет открытием и закрытием аккордиона
+        /// </summary>
+        /// <param name="bindable"></param>
+        /// <param name="oldValue"></param>
+        /// <param name="newValue"></param>
+        private static void IsOpenChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            bool isOpen;
+
+            if (bindable != null && newValue != null)
+            {
+                var control = (Accordion)bindable;
+                isOpen = (bool)newValue;
+
+                if (control.IsOpen == false)
+                {
+                    VisualStateManager.GoToState(control, "Open");
+                    control.Close();
+                }
+                else
+                {
+                    VisualStateManager.GoToState(control, "Closed");
+                    control.Open();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Задает время анимации
+        /// </summary>
+        public uint AnimationDuration { get; set; }
+
+        public Accordion()
+        {
+            InitializeComponent();
+            Close();
+            AnimationDuration = 250;
+            IsOpen = false;
+        }
+
+        /// <summary>
+        /// Закрывает аккордион
+        /// </summary>
+        async void Close()
+        {
+            await Task.WhenAll(
+                _accContent.TranslateTo(0, -10, AnimationDuration),
+                _indicatorContainer.RotateTo(90, AnimationDuration),
+                _accContent.FadeTo(0, 50)
+                );
+            _accContent.IsVisible = false;
+        }
+
+        /// <summary>
+        /// Открывает аккордион
+        /// </summary>
+        async void Open()
+        {
+            _accContent.IsVisible = true;
+            await Task.WhenAll(
+                _accContent.TranslateTo(0, 10, AnimationDuration),
+                _indicatorContainer.RotateTo(0, AnimationDuration),
+                _accContent.FadeTo(30, 50, Easing.SinIn)
+            );
+        }
+
+        /// <summary>
+        /// Реагирует на касание аккордиона
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TitleTapped(object sender, EventArgs e)
+        {
+            IsOpen = !IsOpen;
+        }
+    }
+}
--- Yakutia/Yakutia/Models/ResponseViewModel.cs
+++ Yakutia/Yakutia/Models/ResponseViewModel.cs
@@ -21,21 +21,21 @@ namespace Yakutia.Models
         private void CreateList()
         {
             Briefcase = new HashSet<DataForListModel>();
-            Briefcase.Add(new DataForListModel { TextResponse = "Что такое ТОС (территориальное общественное самоуправление)?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Как получить консультацию по вопросам государственной регистрации некоммерческих организаций?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Как сообщить сведения о создании филиала или представительства некоммерческой организации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Как изменить состав учредителей общественной организации или иной корпоративной некоммерческой организации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Есть ли особенности изменения адреса некоммерческой организации на другой субъект Российской Федерации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Где можно ознакомиться с планом проверок некоммерческих организаций?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Как обжаловать решение территориального органа Минюста России об отказе в государственной регистрации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Как зарегистрировать символику некоммерческой организации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Какой порядок создания общероссийского общественного объединения?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Какие документы должны быть представлены в Минюст России для его регистрации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Какую отчетность общественная организация или общественное движение обязаны представлять в Минюст России (его территориальный орган)?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Как создается и регистрируется политическая партия?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Куда представлять документы для государственной регистрации религиозной организации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Куда оплачивать государственную пошлину за государственную регистрацию религиозной организации?", TextAnswer = "0" });
-            Briefcase.Add(new DataForListModel { TextResponse = "Какие обязанности по предоставлению отчетности установлены для казачьих обществ?", TextAnswer = "0" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Что такое ТОС (территориальное общественное самоуправление)?", TextAnswer = "Ответ 1" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Как получить консультацию по вопросам государственной регистрации некоммерческих организаций?", TextAnswer = "Ответ 2" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Как сообщить сведения о создании филиала или представительства некоммерческой организации?", TextAnswer = "Ответ 3" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Как изменить состав учредителей общественной организации или иной корпоративной некоммерческой организации?", TextAnswer = "Ответ 4" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Есть ли особенности изменения адреса некоммерческой организации на другой субъект Российской Федерации?", TextAnswer = "Ответ 5" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Где можно ознакомиться с планом проверок некоммерческих организаций?", TextAnswer = "Ответ 6" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Как обжаловать решение территориального органа Минюста России об отказе в государственной регистрации?", TextAnswer = "Ответ 7" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Как зарегистрировать символику некоммерческой организации?", TextAnswer = "Ответ 8" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Какой порядок создания общероссийского общественного объединения?", TextAnswer = "Ответ 9" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Какие документы должны быть представлены в Минюст России для его регистрации?", TextAnswer = "Ответ 10" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Какую отчетность общественная организация или общественное движение обязаны представлять в Минюст России (его территориальный орган)?", TextAnswer = "Ответ 11" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Как создается и регистрируется политическая партия?", TextAnswer = "Ответ 12" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Куда представлять документы для государственной регистрации религиозной организации?", TextAnswer = "Ответ 13" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Куда оплачивать государственную пошлину за государственную регистрацию религиозной организации?", TextAnswer = "Ответ 14" });
+            Briefcase.Add(new DataForListModel { TextResponse = "Какие обязанности по предоставлению отчетности установлены для казачьих обществ?", TextAnswer = "Ответ 15" });
         }
 
     }
